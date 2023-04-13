c7f8f7ecdd38876b284b71edf1423e5cc1ab255d
damianolagana <damianolagana@gmail.com>
2023-04-13 15:20:40 +0200

    first inputs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert("ciao")
+let chilometri = prompt("Quanti chilometri vuoi percorrere?") 
+let età = prompt("Quanti anni hai?")
+
+const prezzoKm = "0.21 €"
+
+let prezzoBiglietto = prezzoKm * chilometri
