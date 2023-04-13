7a2a1196255c482857420478d8459ec0986b2d39
damianolagana <damianolagana@gmail.com>
2023-04-13 16:26:32 +0200

    discount età

--- js/main.js
+++ js/main.js
@@ -1,17 +1,26 @@
+const prezzoKm = "0.21" 
+
+
 let chilometri = Number(prompt("Quanti chilometri vuoi percorrere?")) 
+let prezzoBiglietto = prezzoKm * chilometri
+
 
 if (chilometri == null || chilometri == ""){
     alert("Non hai inserito i chilometri");
-} else if (chilometri == Number){
-    alert("Il prezzo del biglietto è" (prezzoBiglietto))
-}
+} 
  
 
 
 
 let età = Number(prompt("Quanti anni hai?")) 
 
-const prezzoKm = "0.21" 
+if (età >= 18 && età < 65){
+    prezzoBiglietto = (prezzoKm * chilometri) * 0.2
+} else if (età > 65){
+    prezzoBiglietto = (prezzoKm * chilometri) * 0.4
+
+}
 
 
-let prezzoBiglietto = prezzoKm * chilometri
+
+
