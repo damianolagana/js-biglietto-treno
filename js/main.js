cf85c79081ccea92aa4f85a639669aff3d59ece1
damianolagana <damianolagana@gmail.com>
2023-04-14 11:10:39 +0200

    prezzo 2 decimali

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const prezzoKm = "0.21" 
 
 let chilometri = Number(prompt("Quanti chilometri vuoi percorrere?")) 
+let età = Number(prompt("Inserisci la tua età")) 
+
 let prezzoBiglietto = prezzoKm * chilometri
 
 
@@ -10,22 +12,26 @@ if (chilometri == null || chilometri == ""){
 } 
  
 
-
-
-let età = Number(prompt("Quanti anni hai?")) 
-let scontoMinori = prezzoBiglietto * 0.2
-let scontoOver65 = prezzoBiglietto * 0.4
-
-if (età < 18 ){
-    prezzoBiglietto = prezzoBiglietto - scontoMinori;
-    alert("Il prezzo del biglietto è " + prezzoBiglietto + "€");
-} else if (età > 65){
-    prezzoBiglietto = prezzoBiglietto - scontoOver65;
-    alert("Il prezzo del biglietto è " + prezzoBiglietto + "€");
-
+if( isNaN(età)){
+    alert("L'età è errata. Non verranno applicate scontistiche")
 } else {
-    alert("Il prezzo del biglietto è " + prezzoBiglietto + "€");
+    let scontoMinori = prezzoBiglietto * 0.2
+    let scontoOver65 = prezzoBiglietto * 0.4
+
+    if (età < 18 ){
+        prezzoBiglietto -= scontoMinori;
+        alert("Il prezzo del biglietto è " + prezzoBiglietto.toFixed(2) + "€");
+    } else if (età > 65){
+        prezzoBiglietto -= scontoOver65;
+        alert("Il prezzo del biglietto è " + prezzoBiglietto.toFixed(2) + "€");
+
+    } else {
+        alert("Il prezzo del biglietto è " + prezzoBiglietto.toFixed(2) + "€");
+    }
+
 }
+
+
 
 
 
